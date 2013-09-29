f396d0bd04600b76de79fcfb5ac33cfd893553f2
OpenERP File User <erp_super@zupy.(none)>
2013-09-29 18:24:19 +0000

    added indexation language

--- solerp/solerp.py
+++ solerp/solerp.py
@@ -1,6 +1,7 @@
 from openerp.osv import fields, osv, orm
 from openerp.osv.orm import Model, TransientModel, AbstractModel
 from openerp.tools.translate import _
+from openerp import SUPERUSER_ID
 import sunburnt
 
 class solr_config_settings(TransientModel):
@@ -10,7 +11,12 @@ class solr_config_settings(TransientModel):
 
     _columns = {
         'solr_url' : fields.char('SolR URL', size=128),
+        'solr_language_id': fields.many2one('res.lang', 'Language')
     }
+
+    def get_default_solr_language_id(self, cr, uid, field_names, context=None):
+        solr_language_id = self.pool.get("ir.config_parameter").get_param(cr, uid, "solr.language_id", context=context)
+        return {'solr_language_id': solr_language_id}
 
     def get_default_solr_url(self, cr, uid, field_names, context=None):
         solr_url = self.pool.get("ir.config_parameter").get_param(cr, uid, "solr.url", context=context)
@@ -20,9 +26,10 @@ class solr_config_settings(TransientModel):
         config_parameters = self.pool.get("ir.config_parameter")
         for record in self.browse(cr, uid, ids, context=context):
             config_parameters.set_param(cr, uid, "solr.url", record.solr_url or '', context=context)
+            config_parameters.set_param(cr, uid, "solr.language_id", record.solr_language_id and record.solr_language_id.id, context=context)
         return True
 
-    def get_solr_interface(self, cr, uid, context=None):
+    def get_solr_interface(self, cr, uid, context=None): #TODO hook to specify collection
         solr_url = self.pool.get("ir.config_parameter").get_param(cr, uid, "solr.url", context=context)
         if solr_url:
             return sunburnt.SolrInterface(solr_url.encode('utf-8'))
@@ -81,12 +88,10 @@ class solr_mixin(AbstractModel):
                 solr_vals["%s_s" % (field,)] = val_name
 
             if field in included_relations:
-                print "IIIIIIIIIIII including field..."
                 field_res_id = solr_vals["%s_i" % (field,)]
                 new_vals = obj.read(cr, uid, [field_res_id], obj.fields_get(cr, uid, context=context).keys())[0]
                 solr_values = obj.oe_to_solr(cr, uid, field_res_id, new_vals, field, context)
                 for rel_k in solr_values.keys():
-                    print "rel_k", rel_k
                     if rel_k != "id" and rel_k != "text":
                         solr_vals["%s_%s" % (field, rel_k)] = solr_values[rel_k]
 
@@ -104,7 +109,6 @@ class solr_mixin(AbstractModel):
         solr_vals["id"] = self._name.replace(".", "-") + "-" + str(res_id)
         solr_vals["text"] = oe_vals.get(self._rec_name)
         for field, descriptor in self.fields_get(cr, uid, context=context).iteritems():
-            print "HHHHHHHHHH", field, descriptor
             solr_vals = self._field_to_solr(cr, uid, field, descriptor['type'], descriptor.get('relation'), included_relations, oe_vals, solr_vals, context)
         return solr_vals
 
@@ -112,9 +116,12 @@ class solr_mixin(AbstractModel):
         res_id = super(solr_mixin, self).create(cr, uid, values, context=context)
         si = self.pool.get('solr.config.settings').get_solr_interface(cr, uid, context)
         if si:
-            new_vals = self.read(cr, uid, [res_id], self.fields_get(cr, uid, context=context).keys())[0]
+            solr_context = context.copy()
+            solr_language_id = self.pool.get("ir.config_parameter").get_param(cr, uid, "solr.language_id", context=context)
+            if solr_language_id:
+                solr_context.update({'lang': self.pool.get('res.lang').read(cr, SUPERUSER_ID, [solr_language_id], ['iso_code'])[0]['iso_code']})
+            new_vals = self.read(cr, uid, [res_id], self.fields_get(cr, uid, context=solr_context).keys(), context=solr_context)[0]
             solr_values = self.oe_to_solr(cr, uid, res_id, new_vals, False, context)
-#            print "*********** CCCCCCCCCCCCC", self._name, solr_values
             si.add(solr_values)
             si.commit()
         return res_id
@@ -124,13 +131,18 @@ class solr_mixin(AbstractModel):
             ids = [ids]
         res = super(solr_mixin, self).write(cr, uid, ids, values, context=context)
         si = self.pool.get('solr.config.settings').get_solr_interface(cr, uid, context)
+        solr_context = False
         if si:
             for id in ids:
-                new_vals = self.read(cr, uid, [id], self.fields_get(cr, uid, context=context).keys())[0]
+                if not solr_context:
+                    solr_context = context and context.copy() or {}
+                    solr_language_id = self.pool.get("ir.config_parameter").get_param(cr, uid, "solr.language_id", context=context)
+                    if solr_language_id:
+                        solr_context.update({'lang': self.pool.get('res.lang').read(cr, SUPERUSER_ID, [solr_language_id], ['iso_code'])[0]['iso_code']})
+                new_vals = self.read(cr, uid, [id], self.fields_get(cr, uid, context=solr_context).keys(), context=solr_context)[0]
                 solr_values = self.oe_to_solr(cr, uid, id, new_vals, False, context)
-#                print "*********** WWWWWWWWWWWWW", self._name, solr_values
                 si.add(solr_values)
-            si.commit()
+                si.commit()
         return res
 
     def unlink(self, cr, uid, ids, context=None):
